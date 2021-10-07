ac6b663fba5215272e6bc4a94b0127b85169f87d
therealAlex <alx.cortez.santos@gmail.com>
2021-10-07 12:26:03 +0100

    first commit

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import mainAudio as mA
+#Dimensao da matriz
+a = np.array([[1,2],[2,3]])
+(l,c) = a.shape
+
+#Meter numa linha só
+b= a.flatten()
+
+#Redimensionar matriz
+a=np.array([[1,2,3,4]])
+b=a.reshape((2,2))
+
+#Mostrar partes de Matriz
+b = [1,2,3,4]
+#print(b[::])
+#print(b[::2])
+
+#Copiar uma matriz
+a = np.array([[1,2],[3,4]])
+#b = a --> como se fosse um ponteiro em c
+b=a.copy() # Mantem uma copia para si mesmo
+a[0,0] = 10
+a[1,1] = b[0,0]
+
+#Multiplicacao de matrizes
+a = np.array([[1,2],[3,4]])
+b=a.copy()
+c = np.dot(a,b) # <=> a*b
+
+#Somatorio mediante a orientação dos eixos --> array.func(axis = 0/1)
+# axis = 1 --> orientação em linha
+# axis = 0 --> orientação em coluna
+b = a.sum(0)
+b = a.sum(1)
+
+#Situações com uma condição aplicad
+print(a)
+print(a>1)
+print(a[a>1])
+
+
+if __name__ == '__main__':
+    mA.start()
