3a8dd7360637f183bed9eab699e759055ead5fad
therealAlex <alx.cortez.santos@gmail.com>
2021-10-16 12:39:27 +0100

    Participation Data extraction to Numpy Array

--- src/importData.py
+++ src/importData.py
@@ -1,1 +1,10 @@
 import csv
+import numpy as np
+
+def extractPartData(dir,numPart):
+    fullDir = dir + str(numPart) + "/part"+str(numPart) +"dev"
+    partData = np.genfromtxt(fullDir+ "1.csv",delimiter=',')
+    for sens in range(2,6):
+        sensData = np.genfromtxt(fullDir+str(sens)+ ".csv",delimiter=',')
+        partData = np.append(partData,sensData,axis=0)
+    return partData
